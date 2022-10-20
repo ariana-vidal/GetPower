d422e7eee21e677a0c9e6d3a5eabd295e2de03d0
Ariana Vidal <barbosavidala@gmail.com>
2022-10-19 22:51:54 -0300

    feat: implementando o sidebar

--- teste-getpower/src/routes/appRoutes.jsx
+++ teste-getpower/src/routes/appRoutes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import Courselist from '../screens/CourseList';
 import Login from '../screens/Login';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
+      <Route path="/courselist" element={ <Courselist /> } />
     </Routes>
   );
 }
