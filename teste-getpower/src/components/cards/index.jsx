f4e88770ded33e9af349feeae575134a787bd917
Ariana Vidal <barbosavidala@gmail.com>
2022-10-21 09:54:27 -0300

    readme e implementaçao das rotas

--- teste-getpower/src/components/cards/index.jsx
+++ teste-getpower/src/components/cards/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 // import list from './list';
 
@@ -19,43 +20,55 @@ export default function Cards() {
   //   </div>
 
     // ))}
-    <div className={ styles.cards }>
-      <section>
-        <span>R$:197.00</span>
-        <span><b>Contratos</b></span>
-        <p>Utilize nossa plataforma de contratos e otimize sua rotina!</p>
-        <img src="https://app.academiadosol.com.br/uploads/cursos/9/banner.png" alt="" />
-        <button
-          type="button"
-        >
-          Acessar
-        </button>
+    <>
+      <div className={ styles.cards }>
+        <section>
+          <span>R$:197.00</span>
+          <span><b>Contratos</b></span>
+          <p>Utilize nossa plataforma de contratos e otimize sua rotina!</p>
+          <img src="https://app.academiadosol.com.br/uploads/cursos/9/banner.png" alt="" />
+          <button
+            type="button"
+          >
+            Acessar
+          </button>
 
-      </section>
-      <section>
-        <span>R$:347.00</span>
-        <span><b>Máquina de vendas</b></span>
-        <p>Transforme em uma máquina de vendas.</p>
-        <img src="https://app.academiadosol.com.br/uploads/cursos/8/maquina_de_vendas.png" alt="" />
-        <button
-          type="button"
-        >
-          Acessar
-        </button>
+        </section>
+        <section>
+          <span>R$:347.00</span>
+          <span><b>Máquina de vendas</b></span>
+          <p>Transforme em uma máquina de vendas o seu negócio.</p>
+          <img src="https://app.academiadosol.com.br/uploads/cursos/8/maquina_de_vendas.png" alt="" />
+          <button
+            type="button"
+          >
+            Acessar
+          </button>
 
-      </section>
-      <section>
-        <span>R$:997.00</span>
-        <span><b>Mestre Fotovoltaico 2.0</b></span>
-        <p>Do zero a especialista. Comece a ter resultados em até 45 dias.</p>
-        <img src="https://app.academiadosol.com.br/uploads/cursos/3/banner.png" alt="" />
-        <button
-          type="button"
-        >
-          Acessar
-        </button>
+        </section>
+        <section>
+          <span>R$:997.00</span>
+          <span><b>Mestre Fotovoltaico 2.0</b></span>
+          <p>Do zero a especialista. Comece a ter resultados em até 45 dias.</p>
+          <img src="https://app.academiadosol.com.br/uploads/cursos/3/banner.png" alt="" />
+          <button
+            type="button"
+          >
+            Acessar
+          </button>
 
-      </section>
-    </div>
+        </section>
+      </div>
+      <Link
+        to="/register"
+      >
+        <button className={ styles.button } type="button">Cadastrar proposta</button>
+      </Link>
+      <Link
+        to="/"
+      >
+        <button className={ styles.button } type="button">Sair</button>
+      </Link>
+    </>
   );
 }
